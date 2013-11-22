50af50b621b151c2925940c5e115ed321b42ecd8
James Routley <jroutley@gmail.com>
2013-11-22 12:31:42 +0000

    22/11/13 12.31
    As before, .m~ files removed

--- Matlab/GramSchmidt/gs_script.m
+++ Matlab/GramSchmidt/gs_script.m
@@ -9,12 +9,10 @@ n = 2;
 
 %generate a matrix V, columns of which are monomials over the range of x
 V = zeros(length(x), n);
-
 for i = 1:n+1
     for j = 1:length(x)       
         V(j,i) = x(j)^(i-1);
     end
 end
-
 V
 
