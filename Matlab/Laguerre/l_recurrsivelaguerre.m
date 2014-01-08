047c77821119fd3d3bd714fb3fd64e3aa7642179
James Routley <jroutley@gmail.com>
2014-01-08 00:42:30 +0000

    remove .m

--- Matlab/Laguerre/l_recurrsivelaguerre.m
+++ Matlab/Laguerre/l_recurrsivelaguerre.m
@@ -15,12 +15,11 @@ a = 0;
 C = l_recurrsivelaguerrecoefficients(n, a);
 
 
-
         %Script substitutes in numerical values for the x terms and stores
         %numerical values of the Laguerre polynomials in L
 
         %range of x axis values
-xrange = linspace(0, 10, 11);
+xrange = linspace(-5, 20, 1001);
 
 X = zeros(n, length(xrange));
 for i = 1:n
@@ -28,25 +27,20 @@ for i = 1:n
     X(i, :) = xrange.^(i-1);
     
 end    
-C
-X
 
+%flip C left-right 
+D = fliplr(C);
+L = D*X;
 
-L = C*X;
-
-
+%plot
 hold off ; set ( gca , 'FontSize' , 18);
 plot(xrange,L,'-', 'LineWidth' ,2)
 legend ('g0','g1','g2','g3','g4','g5', 'Location', 'NorthWest') 
 xlabel('x-values');
 ylabel(sprintf('Normalised Gram-Schmidt vectors up to %d terms',n));
 title('Normalised Gram-Schmidt vectors');
-axis ([0 10 -20 130]);
+axis ([-5 20 -15 30]);
 
-
-%plot(xrange, L)
-
-%plot(L, xrange)
 
 
 
